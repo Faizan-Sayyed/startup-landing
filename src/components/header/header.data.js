d1af7ab8c77513da5b9a6542699969f2278a6a67
Faizan-Sayyed <faizansayyed0210@gmail.com>
2022-01-18 13:39:05 +0530

    initial commit

---
+++ src/components/header/header.data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    path: 'home',
+    label: 'Home',
+  },
+  {
+    path: 'connect',
+    label: 'Connect',
+  },
+  {
+    path: 'product',
+    label: 'Product',
+  },
+  {
+    path: 'team',
+    label: 'Team',
+  },
+  {
+    path: 'testimonials',
+    label: 'Testimonials',
+  },
+];
